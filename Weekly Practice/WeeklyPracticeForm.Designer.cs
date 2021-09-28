6dd49772a03651dbbf8071d4898c344e271f7efc
Zachary Frey <zacharypfrey@gmail.com>
2021-09-28 15:07:36 -0500

    Add a test

--- Weekly Practice/WeeklyPracticeForm.Designer.cs
+++ Weekly Practice/WeeklyPracticeForm.Designer.cs
@@ -31,6 +31,7 @@ namespace Weekly_Practice
         {
             this.startButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // startButton
@@ -53,12 +54,23 @@ namespace Weekly_Practice
             this.exitButton.UseVisualStyleBackColor = true;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(342, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Test";
+            // 
             // WeeklyPracticeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(781, 502);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.startButton);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -66,12 +78,14 @@ namespace Weekly_Practice
             this.Name = "WeeklyPracticeForm";
             this.Text = "Weekly Practice Form";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
