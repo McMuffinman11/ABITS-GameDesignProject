5b4e7bca4cad83c7299ccb578a777b9e8ebf8eea
Zachary Frey <zacharypfrey@gmail.com>
2021-10-01 17:08:42 -0500

    Major updates

--- Creative Name/Creative Name/Game1.cs
+++ Creative Name/Creative Name/Game1.cs
@@ -5,13 +5,16 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Media;
+
 
 // Get absolute value from times, then give a score based on difference
 // Display
 namespace Creative_Name
 {
     public partial class Game1 : Form
-    { Random rand = new Random();
+    {
+        Random rand = new Random();
         int targetTime;
         int counter;
         int absoluteTimeDifference;
@@ -23,6 +26,9 @@ namespace Creative_Name
         public Game1()
         {
             InitializeComponent();
+            confetti.Visible = false;
+            angry.Visible = false;
+            music();
             targetTime = rand.Next(0, 1000);
             RandomTimeLabel.Text = targetTime.ToString();
         }
@@ -33,6 +39,7 @@ namespace Creative_Name
             CurrentTimeLabel.Text = counter.ToString();
         }
 
+
         private void Game1_MouseClick(object sender, MouseEventArgs e)
         {
             Game1Timer.Stop();
@@ -40,18 +47,26 @@ namespace Creative_Name
             if (absoluteTimeDifference <= 5)
             {
                 Score = 20;
+                confetti.Visible = true;
+                laugh();
             }
-            else if(absoluteTimeDifference <= 30 && absoluteTimeDifference > 5)
+            else if (absoluteTimeDifference <= 30 && absoluteTimeDifference > 5)
             {
                 Score = 15;
+                confetti.Visible = true;
+                laugh();
             }
             else if (absoluteTimeDifference > 30 && absoluteTimeDifference <= 50)
             {
                 Score = 10;
+                confetti.Visible = true;
+                laugh();
             }
             else
             {
                 Score = 5;
+                angry.Visible = true;
+                grumble();
             }
             string scoreMessage = "Score: " + Score;
             ScoreLabel.Text = scoreMessage;
@@ -63,6 +78,24 @@ namespace Creative_Name
             int randomGreen = greenRand.Next(230, 256);
             int randomBlue = blueRand.Next(230, 256);
             this.BackColor = Color.FromArgb(randomRed, randomGreen, randomBlue);
+        }
+
+        private void laugh()
+        {
+            SoundPlayer player = new SoundPlayer(@"C:\Users\zackfrey\Desktop\Project Sounds\laugh.wav");
+            player.Play();
+        }
+
+        private void music()
+        {
+            SoundPlayer player = new SoundPlayer(@"C:\Users\zackfrey\Desktop\Project Sounds\music.wav");
+            player.Play();
+        }
+
+        private void grumble()
+        {
+            SoundPlayer player = new SoundPlayer(@"C:\Users\zackfrey\Desktop\Project Sounds\grumble.wav");
+            player.Play();
         }
     }
 }
